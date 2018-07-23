bdb0cdc53fcc26238a3944e2576e9c176c4f5c3e
Gökhan Kanber <GokhanKanber@users.noreply.github.com>
2018-07-23 17:11:21 +0300

    Create BaseInputController.java

---
+++ app/src/main/java/com/gokhankanber/spaceinvaders/controller/BaseInputController.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2018 Gökhan Kanber
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gokhankanber.spaceinvaders.controller;
+
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector3;
+
+public abstract class BaseInputController implements InputProcessor
+{
+    protected OrthographicCamera camera;
+    protected Vector3 touchPoint1;
+
+    public BaseInputController(OrthographicCamera camera)
+    {
+        this.camera = camera;
+        touchPoint1 = new Vector3();
+    }
+
+    @Override
+    public boolean keyDown(int keycode)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount)
+    {
+        return false;
+    }
+}
