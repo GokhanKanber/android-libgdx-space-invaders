ad1f5af0a5c53dfeb8d3f98c69802c1bf4306ad1
Gökhan Kanber <GokhanKanber@users.noreply.github.com>
2018-07-23 17:05:52 +0300

    Create MainFragment.java

---
+++ app/src/main/java/com/gokhankanber/spaceinvaders/MainFragment.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018 Gökhan Kanber
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gokhankanber.spaceinvaders;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.backends.android.AndroidFragmentApplication;
+
+public class MainFragment extends AndroidFragmentApplication
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useWakelock = true;
+
+        return initializeForView(new SpaceInvaders(getResources()), config);
+    }
+}
